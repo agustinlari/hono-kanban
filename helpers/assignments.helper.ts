f4cb8e270cfef86be9e525efad5dca9de367389b
agustinlari <agustin.lago@osmos.es>
2025-10-31 07:58:26 +0100

    ok

--- helpers/assignments.helper.ts
+++ helpers/assignments.helper.ts
@@ -147,7 +147,7 @@ class AssignmentService {
               const { emailService } = await import('../services/email.service');
               const { emailSettings } = await import('../config/email.config');
 
-              const cardUrl = `${emailSettings.appUrl}/kanban?board=${cardData.board_id}&card=${cardId}`;
+              const cardUrl = `${emailSettings.appUrl}/kanban/?board=${cardData.board_id}&card=${cardId}`;
 
               await emailService.sendCardAssignedNotification({
                 userEmail: userData.email,
